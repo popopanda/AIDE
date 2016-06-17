da32c8b41deffc03a5a1c22f6964e914f3421964
Jeffrey Leung <jeffrey.leung@fundingcircle.com>
2016-06-17 22:23:51 +0000

    cron script change mail to variable

--- files/default/cron_aide.sh
+++ files/default/cron_aide.sh
@@ -2,9 +2,10 @@
 
 MYDATE=$(date +%Y-%m-%d)
 MYFILENAME="AIDE-"${MYDATE}.txt
+MAILTO=""
 
 /bin/echo "Aide check !! $(date)" > /tmp/$MYFILENAME
 sudo /usr/bin/aide.wrapper --check > /tmp/$MYFILENAME
 /bin/echo "****************DONE******************" >> /tmp/$MYFILENAME
-/usr/bin/mail -s"$MYFILENAME `date`" <insert email here> < /tmp/$MYFILENAME
+/usr/bin/mail -s"$MYFILENAME `date`" $MAILTO < /tmp/$MYFILENAME
 /bin/rm /tmp/myAide.txt
