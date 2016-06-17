9bec3371fc05949317bb4cae2ea9c6f2ce6b5d7d
Jeffrey Leung <jeffrey.leung@fundingcircle.com>
2016-06-17 15:50:00 -0700

    update metadata

--- Berksfile
+++ Berksfile
@@ -1,5 +1,3 @@
 source 'https://supermarket.chef.io'
 
 metadata
-
-cookbook 'cron', '~> 1.7.6'
--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license 'all_rights'
 description 'Installs/Configures aide'
 long_description 'Installs/Configures aide'
 version '0.1.0'
+
+depends 'cron', '~> 1.7.6'
